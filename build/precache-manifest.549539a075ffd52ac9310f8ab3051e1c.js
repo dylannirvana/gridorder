ff0501b1ef6580e690d43c51dbe4130eb258b7e1
Dylan Nirvana <dylannirvana@Dylans-MBP-2.cl.local>
2019-07-12 11:16:06 -0400

    adds build folder correct gitignore

---
+++ build/precache-manifest.549539a075ffd52ac9310f8ab3051e1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3478c2f39e974ba84465ab2d37da7570",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2bd98d14c2f2e4f175c",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "415786f5e2792deee961",
+    "url": "/static/css/main.3e8e552c.chunk.css"
+  },
+  {
+    "revision": "c2bd98d14c2f2e4f175c",
+    "url": "/static/js/2.e677b6cb.chunk.js"
+  },
+  {
+    "revision": "415786f5e2792deee961",
+    "url": "/static/js/main.209b76c6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
